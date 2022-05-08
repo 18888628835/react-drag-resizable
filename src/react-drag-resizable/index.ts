e289a4b3e75af285acee422853616368ef75a374
qiuyanxi <qiuyanxi1992@gmail.com>
2022-05-08 23:27:30 +0800

    重新导出默认

--- src/react-drag-resizable/index.ts
+++ src/react-drag-resizable/index.ts
@@ -1,8 +1,6 @@
-import DragResizableBox from './drag-resizable-box';
-
 export type {
   DragResizableBoxProps,
   CollectedRectType,
 } from './drag-resizable-box';
 
-export default DragResizableBox;
+export { default } from './drag-resizable-box';
